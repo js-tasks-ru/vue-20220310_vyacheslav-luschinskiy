e7c8e248faa282a288db4b58610f7d4dadf615cd
Viacheslav Lushchinskiy <viacheslav.lushchinskiy@swissmarketplace.group>
2022-03-16 19:43:34 +0100

    Calculator app done

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,30 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const myApp = createApp({
+  data() {
+    return {
+      firstNumber: 0,
+      secondNumber: 0,
+      operator: 'sum',
+    };
+  },
+  computed: {
+    calculate() {
+      if (this.operator === 'sum') {
+        return this.firstNumber + this.secondNumber;
+      }
+      if (this.operator === 'subtract') {
+        return this.firstNumber - this.secondNumber;
+      }
+      if (this.operator === 'multiply') {
+        return this.firstNumber * this.secondNumber;
+      }
+      if (this.operator === 'divide') {
+        return this.firstNumber / this.secondNumber;
+      }
+      return 0;
+    },
+  },
+});
+
+myApp.mount('#app');
