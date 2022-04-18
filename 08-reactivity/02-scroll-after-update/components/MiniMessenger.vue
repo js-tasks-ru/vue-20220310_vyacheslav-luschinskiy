a818d154f1e65df24334d6d5c07ee78f99e8ab38
Slava <slaval@mail.ru>
2022-04-18 09:53:46 +0200

    nextTick added

--- 08-reactivity/02-scroll-after-update/components/MiniMessenger.vue
+++ 08-reactivity/02-scroll-after-update/components/MiniMessenger.vue
@@ -34,7 +34,7 @@ export default {
   methods: {
     handleSendSubmit() {
       this.send();
-      this.$refs.items.at(-1).scrollIntoView();
+      this.$nextTick(() => this.$refs.items.at(-1).scrollIntoView());
     },
 
     send() {
